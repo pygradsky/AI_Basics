c06b13139a6b491c9477209ddc0551bd6f55d1e8
Fedor <feda.vert@gmail.com>
2026-04-15 14:37:42 +0300

    update simulator

--- OOP/Simulator.py
+++ OOP/Simulator.py
@@ -1,10 +1,43 @@
 class Game:
-    CORN_PRICE: int = 10
-    COW_PRICE: int = 100
+    CORN_PRICE: float = 5.0
+    COW_PRICE: float = 45.0
     
 
-    def __init__(self):
-        pass
+    def __init__(self, balance=100.0):
+        self.balance = balance
+    
+    @staticmethod
+    def validate_balance(balance: float, item_price: float):
+        if balance - item_price < 0:
+            return False
+        return True
 
-    def buy(self, type):
-        pass
+    def buy(self, type: str, amount: int = 1):
+        # Cow
+        if type == "cow":
+            item_price = Game.COW_PRICE * amount
+
+            if Game.validate_balance(self.balance, item_price):
+                self.balance -= item_price
+                print(f"Вы купили корову за {item_price} монет. Баланс: {self.balance}")
+            else:
+                print("На балансе не достаточно средств!")
+        # Sheep
+        elif type == "sheep":
+            pass
+        # Corn
+        elif type == "corn":
+            item_price = Game.CORN_PRICE * amount
+
+            if Game.validate_balance(self.balance, item_price):
+                self.balance -= item_price
+                print(f"Вы купили зерно за {item_price} монет. Баланс: {self.balance}")
+            else:
+                print("На балансе не достаточно средств!")
+                
+
+game = Game()
+game.buy("cow", 2)
+game.buy("corn")
+
+print(game.balance)
